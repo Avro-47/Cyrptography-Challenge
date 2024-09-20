cac52e42b78dd25fc660c829856f66e1bb33e4d7
Avro-47 <Avro47@proton.me>
2024-09-19 23:58:50 -0600

    Completed Challenge 1
    converted hex into base64 ( readable text )

---
+++ Set 1/Challegne 1.py
@@ -0,0 +1,5 @@
+import codecs
+
+hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
+base64 = codecs.encode(codecs.decode(hex, 'hex'), 'base64').decode()
+print(base64)
