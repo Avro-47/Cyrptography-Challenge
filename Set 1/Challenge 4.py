69eb4c66430be7c51077e8e7b4a0b50910ffbfd8
Avro-47 <Avro47@proton.me>
2024-09-25 19:45:31 -0600

    Progressed on challenge 4 set 1
    Now prints its output to a file, including the string that it selected, the xor_key, and then finally the output itself.

--- Set 1/Challenge 4.py
+++ Set 1/Challenge 4.py
@@ -1,18 +1,27 @@
 import binascii
 from os import close
+from shlex import join
 from venv import create
 
-def Xor_Check(encoded: str):
+def Xor_Check(encoded: str, number: int):
     mystr = bytes.fromhex(encoded)
     for xor_key in range(256):
         decoded = ''.join(chr(b ^ xor_key) for b in mystr)
         if decoded.isprintable() and decoded.isascii():
-            
 
-            print(xor_key, decoded)        #    <<--|
-#TODO                                      #        |
-#Print a space between sets of strings.    #        |
-#print to Challenge4Output rahter then cmt ---->>>> |
+
+
+            # my work around for printing the key, line that the cipher was on, plus decoded message in one line
+            OUTPUT = ''.join(str(xor_key))
+            OUTPUT += ''.join("  ")
+            OUTPUT += ''.join(str(i))
+            OUTPUT += ''.join("  ")
+            OUTPUT += ''.join(decoded)
+            OUTPUT += '\n'
+            #print(OUTPUT)
+            OutputData.write(OUTPUT)           
+#TODO                                             
+#Print a space between sets of strings. 
 #verify that it found an actually awnser and has not been all for nought. 
 
 OutputData = open(r"Challenge4Output","w")
@@ -20,7 +29,7 @@ OutputData = open(r"Challenge4Output","w")
 # lines = len(Data.readlines())
 with open('D:\Code files\GItRepo\Cyrptography-Challenge\Set 1\Challenge4data') as f: #This goes through ALL lines
     for i, line in enumerate(f, start=1):                                            #This goes through ALL lines
-        Xor_Check(line)
+        Xor_Check(line, i)
         
 
 
